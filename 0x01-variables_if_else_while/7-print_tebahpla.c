29313bf91b9a07ecc4393a80b5986c76f463ba79
Moses Sunday Scot <sunday.moses12@gmail.com>
2023-02-17 23:19:13 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase in reverse order
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'z'; lc <= 'a'; lc--)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
