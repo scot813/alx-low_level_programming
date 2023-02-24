fef9a712014d70be38999f6ed2f1cb42bf76c773
Moses Sunday Scot <sunday.moses12@gmail.com>
2023-02-23 17:27:18 -0800

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -19,8 +19,10 @@ int largest_number(int a, int b, int c)
 	else if (b > c && b > a)
 	{
 		largest = b;
+	}
 	else if (c > b)
-		largest = c
+	{
+		largest = c;
 	}
 	else
 	{
