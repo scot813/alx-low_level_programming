57b31ffb355d7d10383f0dbe6218019ffe9d2206
Moses Sunday Scot <sunday.moses12@gmail.com>
2023-02-23 19:31:30 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multilying 2 int
+ * @a: first int
+ * @b: thye second int
+ * Return: addition of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a + b);
+}
