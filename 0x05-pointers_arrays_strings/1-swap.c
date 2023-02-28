4217e411c1f6d1cc2acf3e37ecaba9d3138b87d6
Moses Sunday Scot <sunday.moses12@gmail.com>
2023-02-28 19:17:56 +0100

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swapa the value of two inetgers
+ * @a: integer to sawp
+ * @b: integer to swap
+ */
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	n = *a;
+	*a = *b;
+	*b = m;
+}
