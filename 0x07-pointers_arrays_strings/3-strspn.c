f2005a3cab2b6171bf4a47ea8c10b6099914081d
scot813 <sunday.moses12@gmail.com>
2023-03-06 15:36:25 -0800

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,12 +8,12 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int n=0;
+	unsigned int n = 0;
 	int r;
 
 	while (*s)
 	{
-		for (r =0; accept[r]; r++)
+		for (r = 0; accept[r]; r++)
 		{
 			if (*s == accept[r])
 			{
