dd28ca996f463b18783e6cf71631c9732f3e7da0
scot813 <sunday.moses12@gmail.com>
2023-03-06 14:35:49 -0800

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,12 +9,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i = 0;
-	int r = n;
+	int r = 0;
+	int i = n;
 
-		for (; i > r; i++)
+		for (; r > i; r++)
 		{
-			dest[i] = src[i];
+			dest[r] = src[r];
 			n--;
 		}
 		return (dest);
