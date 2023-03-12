97e5caea0acd19dbbde1ac5c542e12f6ad9d3bcb
scot813 <sunday.moses12@gmail.com>
2023-03-11 18:47:00 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
