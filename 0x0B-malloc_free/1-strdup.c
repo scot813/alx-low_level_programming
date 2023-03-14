11367cbbb4cfbf07f8f8e82fa2bf09eb4dbd3514
scot813 <sunday.moses12@gmail.com>
2023-03-14 16:35:45 -0700

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - entry point
+ * @str: in put value
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	 int j, i= 0;
+
+	 if (str == NULL)
+		 return (NULL);
+
+	j = 0;
+	while (str[j] != '\0')
+		j++;
+	ptr = malloc(sizeof(char) * (j + 1));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for  (i = 0; str[i]; i++)
+		ptr[i] = str[i];
+	return (ptr);
+}
+
