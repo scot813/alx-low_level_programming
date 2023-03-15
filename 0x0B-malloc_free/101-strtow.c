fa5791e004cef846c76d14fdb2bcd31435449736
scot813 <sunday.moses12@gmail.com>
2023-03-14 19:16:32 -0700

    task 6

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,72 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * count_word - helpehr
+ * @s: input
+ * Return: 0
+ */
+int count_word(char *s)
+{
+	int flag, c, w;
+
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+	return (w);
+}
+/**
+ * strtow - entry point
+ * @str: input
+ * Return:0
+ */
+
+char **strtow(char *str)
+{
+	char **m, *t;
+	int i, k = 0, l = 0, words, c = 0, start, end;
+
+	while (*(str + l))
+		l++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	m = (char **) malloc(sizeof(char *) * (words + 1));
+	if (m == NULL)
+		return (NULL);
+
+	for (i = 0; i <= l; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				t = (char *) malloc(sizeof(char) * (c + i));
+				if (t == NULL)
+					return (NULL);
+
+				while (start < end)
+				*t++ = str[start++];
+				*t = '\0';
+				m[k] = t - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+	m[k] = NULL;
+	return (m);
+}
