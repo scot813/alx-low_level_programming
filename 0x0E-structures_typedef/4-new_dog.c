4a9301f0cdb0bbe06e0a3f63466e1e7d255e0a75
scot813 <sunday.moses12@gmail.com>
2023-03-21 04:14:33 -0700

    task 0 - 3

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+/**
+ * new_dog - the dog to be created
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: long 
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t 
