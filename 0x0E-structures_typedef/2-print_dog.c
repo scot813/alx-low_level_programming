df28b6900c335da71a9267a5dcf076ed7a67224d
scot813 <sunday.moses12@gmail.com>
2023-03-21 04:03:07 -0700

    task 0 - 3

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * init_dog - entry point
+ * @d: value
+ * @name: value
+ * @age: third value
+ * @owner: fourth value
+ */
+
+void print_dog(struct dog *d)
+{
+        if (d == NULL)
+                return;
+
+
+	if (d->name == NULL)
+		d->name = "(nill)";
+        if (d->owner == NULL)
+		d->owner = "(nill)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
