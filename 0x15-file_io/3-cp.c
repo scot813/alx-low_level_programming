6f0635b5fe27008671a2846b3ccfaa0d580fc949
scot813 <sunday.moses12@gmail.com>
2023-04-13 10:43:16 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,16 +15,16 @@ int main(int argc, char **argv)
 		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-		dprintf(2, "Error: Can't read from file %s\n", argv[2]), exit(98);
-	file_to = open(argv[2], O_CREAT | O_TRUNC, 0664);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
-		dprintf(2, "Error: Can't write to %s\n", argv[1]), exit(99);
+		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 	fwrite = write(file_to, buffer, 1024);
 	if (fwrite == -1)
-		dprintf(2, "Error: Can't write to %s\n", argv[1]), exit(99);
+		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 	fread = read(file_from, buffer, 1024);
 	if (fread == -1)
-		dprintf(2, "Error: Can't read from file %s\n", argv[2]), exit(98);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	if (close(file_to) == -1)
 		dprintf(2, "Error: Can't close fd %d\n", file_to), exit(100);
 	if (close(file_from) == -1)
