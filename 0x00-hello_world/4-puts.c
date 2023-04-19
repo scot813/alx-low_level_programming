03aeddd64d913f58e01653189acaa30d98ed8d06
scot813 <sunday.moses12@gmail.com>
2023-04-19 08:19:11 -0700

    task 3 and 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+/**
+ * main - entry point
+ *
+ *
+ * Return: 0 on succeess
+ */
+int main(void)
+{
+	 int s;
+
+	s =  puts("Programming is like building a multilingual puzzle\n");
+	return (s);
+	return (0);
+}
