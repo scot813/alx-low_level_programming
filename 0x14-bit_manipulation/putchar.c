1a3121d419a0e30e10caf544af103d1cced96f09
scot813 <sunday.moses12@gmail.com>
2023-07-08 20:53:52 -0700

    mandatory task

---
+++ 0x14-bit_manipulation/putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - write the character c to stdout
+ * @c: the character to be print
+ *
+ * Return: on success 1
+ *0n error, -1 is returned, and errno is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
